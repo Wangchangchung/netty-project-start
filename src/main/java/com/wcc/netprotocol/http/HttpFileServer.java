5f384e9c42c4e058733cd58c7edaf112859a3f89
Wangchangchung <1132629383@qq.com>
2018-03-13 23:59:03 +0800

    netty http开发

---
+++ src/main/java/com/wcc/netprotocol/http/HttpFileServer.java
@@ -0,0 +1,35 @@
+package com.wcc.netprotocol.http;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+public class HttpFileServer {
+
+    private static final String DEFAULT_URL = "";
+
+    public void run(final int port, final String url){
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        socketChannel.pipeline().addLast("http-decoder", new HttpRequestDecoder());
+                        socketChannel.pipeline().addLast("http-aggregator", new HttpObjectAggregator(65536));
+                        socketChannel.pipeline().addLast("http-encoder", new HttpResponseEncoder());
+                        socketChannel.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
+                        socketChannel.pipeline().addLast("fileServerHandler", new )
+                    }
+                });
+    }
+
+}
---
+++ src/main/java/com/wcc/netprotocol/http/HttpFileServerHandler.java
@@ -0,0 +1,11 @@
+package com.wcc.netprotocol.http;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.FullHttpRequest;
+
+public class HttpFileServerHandler extends SimpleChannelInboundHandler<FullHttpRequest>{
+    @Override
+    public void messageReceived(ChannelHandlerContext channelHandlerContext, FullHttpRequest fullHttpRequest) throws Exception {
+    }
+}
